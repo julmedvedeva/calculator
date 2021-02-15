6a98a2cebf4f2f5bf315a9379342b00a1761dcc4
Julia Medvedeva <yu.medvedeva@qlean.ru>
2021-02-15 21:18:14 +0300

    refactor 2

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let result;
 let input1;
 let input2;
 button.onclick = function () {
-  result = document.getElementById('result');
-  input1 = Number(document.getElementById('input1').value);
-  input2 = Number(document.getElementById('input2').value);
+  result = document.querySelector('#result');
+  input1 = Number(document.querySelector('#input1').value);
+  input2 = Number(document.querySelector('#input2').value);
   result.innerHTML = `результат ${input1 + input2}`;
 };
