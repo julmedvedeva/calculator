d431f75a0c6387b14d9b149bb6a35560909b4875
Julia Medvedeva <yu.medvedeva@qlean.ru>
2021-03-31 08:25:36 +0300

    рефактор

--- script.js
+++ script.js
@@ -1,78 +1,73 @@
-let saveRes = document.createElement('div');
+const saveRes = document.createElement('div');
+
 saveRes.id = 'saveRes';
 saveRes.style.height = '100px';
 saveRes.style.width = '100px';
 saveRes.style.backgroundColor = 'red';
 console.log(saveRes);
-let allResults = document.createElement('div');
+
+const allResults = document.createElement('div');
 allResults.id = 'allResults';
 document.body.append(saveRes);
 document.body.append(allResults);
 
-let storage = {
+const storage = {
   _val1: 0,
   _val2: 0,
   result: [],
-  get Val1() {
+
+  get val1() {
     return this._val1;
   },
-  get Val2() {
+  get val2() {
     return this._val2;
   },
-  get ValsSum() {
+  get valsSum() {
     return this._val1 + this._val2;
   },
-  set Val1(val) {
+  set val1(val) {
     if (typeof +val === 'number' && !isNaN(val)) {
-      this._val1 = val;
+      this._val1 = +val;
     } else {
       this._val1 = 0;
     }
   },
-  set Val2(val) {
+  set val2(val) {
     if (typeof +val === 'number' && !isNaN(val)) {
-      this._val2 = val;
+      this._val2 = +val;
     } else {
       this._val2 = 0;
     }
   },
-  safe(key, val) {
-    return (this[key] = val);
+
+  save(key, val) {
+    this[key] = val;
   },
-  print(val1, val2) {
-    document.querySelector('#result').innerHTML = `результат ${+val1 + +val2}`;
-  },
-  update1(key, val) {
-    this.safe(key, val);
-    this.print(this.Val1, this.Val2);
+  update(key, val) {
+    this.save(key, val);
+    this.printCurrentResult();
   },
   printCurrentResult() {
-    return (document.querySelector('#result').innerHTML = `результат ${this.ValsSum}`);
+    document.querySelector('#result').innerHTML = `результат ${this.valsSum}`;
   },
   printAllResults(arr) {
-    let a = this.generateListMarkup(arr);
-    return (allResults.innerHTML = a);
+    allResults.innerHTML = this.generateListMarkup(arr);
   },
   generateListMarkup(arr) {
     let list = arr.map((item) => `<li>${item}</li>`).join('');
     list = `<ul>${list}</ul>`;
     return list;
   },
-  saveResult(val) {
-    if (val > 0) {
-      this.result = this.printCurrentResult(val);
-    } else {
-      alert('Чойта ты тут делаишь?');
-    }
+  updateList() {
+    this.result.push(this.valsSum);
+    this.printAllResults(this.result);
   },
 };
 
 document.querySelector('#input1').addEventListener('keyup', function ({ target: { value } }) {
-  storage.update1('Val1', value);
+  storage.update('val1', value);
 });
 document.querySelector('#input2').addEventListener('keyup', function ({ target: { value } }) {
-  storage.update1('Val2', value);
+  storage.update('val2', value);
 });
-document.querySelector('#saveRes').addEventListener('keyup', function ({ target: { value } }) {
-  storage.saveResult();
-});
+document.querySelector('#saveRes').addEventListener('click', () => storage.updateList());
