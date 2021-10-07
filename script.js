b49dabad5917646826136d8267f3f06d1cb62666
Julia Medvedeva <yu.medvedeva@qlean.ru>
2021-10-07 09:30:25 +0300

    feat: прикрутила лоадер

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const saveRes = document.createElement('div');
 const allResults = document.createElement('div');
 const list = document.createElement('ul');
 const container = document.querySelector('.container');
+const main = document.querySelector('.main');
 const requestUrl = './data.json';
+const result = document.querySelector('#result-div');
 
 allResults.id = 'allResults';
-container.append(saveRes);
-container.append(allResults);
+container.prepend(saveRes);
+main.append(allResults);
 allResults.append(list);
 
 const storage = {
@@ -15,16 +17,18 @@ const storage = {
   items: [],
   ul: document.querySelector('ul'),
 
-  //работа со значением из интупа
   get val1() {
     return this._val1;
   },
+
   get val2() {
     return this._val2;
   },
+
   get valsSum() {
     return this._val1 + this._val2;
   },
+
   set val1(val) {
     if (typeof +val === 'number' && !isNaN(val)) {
       this._val1 = +val;
@@ -32,6 +36,7 @@ const storage = {
       this._val1 = 0;
     }
   },
+
   set val2(val) {
     if (typeof +val === 'number' && !isNaN(val)) {
       this._val2 = +val;
@@ -39,11 +44,41 @@ const storage = {
       this._val2 = 0;
     }
   },
+
   loading: true,
 
   init(data) {
+    console.log(data);
+    if (this.loading === true && data.length === 0) {
+      this.createLoader();
+    }
+
+    // if (this.loading === true && data.length > 0) {
     data.forEach(({ value, id }) => storage.addItem({ value, id }));
     this.loading = false;
+    // }
+  },
+
+  createLoader() {
+    const loadingDiv = document.createElement('div');
+    loadingDiv.classList.add('loading');
+    loadingDiv.innerHTML = `<div class="loading-text">
+      <span class="loading-text-words">L</span>
+      <span class="loading-text-words">O</span>
+      <span class="loading-text-words">A</span>
+      <span class="loading-text-words">D</span>
+      <span class="loading-text-words">I</span>
+      <span class="loading-text-words">N</span>
+      <span class="loading-text-words">G</span>
+      </div>`;
+
+    main.append(loadingDiv);
+  },
+
+  deleteLoader() {
+    const loadingAnimation = document.querySelector('.loading');
+    main.removeChild(loadingAnimation);
+    return this.main;
   },
 
   generateId() {
@@ -60,7 +95,7 @@ const storage = {
   },
 
   printCurrentResult() {
-    document.querySelector('#result').innerHTML = `результат ${this.valsSum}`;
+    result.innerHTML = `Результат сложения: ${this.valsSum}`;
   },
 
   printAllResults() {
@@ -73,32 +108,29 @@ const storage = {
     const button = document.createElement('button');
     button.innerHTML = `<i class="fas fa-backspace"></i>`;
     button.className = 'delete';
+
     updateElementStyles(button, {
       backgroundColor: 'darkmagenta',
       color: 'antiquewhite',
       border: '1px solid',
       borderRadius: '20%',
     });
+
     return button;
   },
 
   createItem({ value, id }) {
-    // Создает элемент вида:
     const item = {
       element: document.createElement('li'),
       id: id || this.generateId(),
       value: value || this.valsSum,
       button: this.generateButtonDelete(),
     };
-    // Тут же установи ему data-id, наполни
     item.element.setAttribute('data-id', item.id);
     item.element.innerHTML = item.value;
     item.element.appendChild(item.button);
-    // его всяким и повесь эвент на его кнопку
     item.button.addEventListener('click', (e) => {
       const parent = e.currentTarget.parentNode;
-      console.log('parent', parent);
-
       this.removeItem(parent);
     });
 
@@ -107,6 +139,7 @@ const storage = {
 
   addItem(value, id) {
     const item = this.createItem(value, id);
+
     updateElementStyles(item.element, {
       listStyle: 'none',
       width: '20%',
@@ -116,9 +149,16 @@ const storage = {
       fontSize: '40px',
       margin: '1%',
     });
+
     this.items.push({ id: item.id, value: item.value });
+    //todo это условие ломает все
+    // if (this.items.length <= 1) {
+    //   this.deleteLoader();
+    // }
+
     this.ul.appendChild(item.element);
   },
+
   removeItem(itemId) {
     const attribute = itemId.getAttribute('data-id');
 
@@ -128,16 +168,19 @@ const storage = {
 
     if (item) {
       this.ul.removeChild(itemId);
-      console.log(this.items, item);
-      const b = this.items.slice(item);
-      console.log(b);
-      // вырезать из массива items этот item
+      const index = this.items.indexOf(item);
+      if (index > -1) {
+        this.items.splice(index, 1);
+      }
+      if (this.items.length === 0) {
+        this.createLoader();
+      }
+      return this.items;
     }
   },
 };
 
 function updateElementStyles(el, propsObj, id) {
-  //вторая часть выполнится если первая вернет true
   id && (el.id = id);
 
   for (let key in propsObj) {
@@ -161,8 +204,9 @@ updateElementStyles(
     height: '100px',
     width: '100px',
     backgroundColor: 'crimson',
-    border: '1px solid crimson',
+    border: '1px solid khaki',
     borderRadius: '13px',
+    marginRight: '10px',
   },
   'saveRes',
 );
